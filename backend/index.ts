4fdfa8de16b90feed310c44a708933ef030cb63d
r800360 <rosachdeva@ucsd.edu>
2025-02-27 13:39:12 -0800

    Clean up backend and add payments to index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,15 @@
 import dotenv from "dotenv";
 import express, { Express, NextFunction, Request, Response } from "express";
 import { isHttpError } from "http-errors";
-
-import announcementRoutes from "./src/routes/announcement";
-import discussionRoutes from "./src/routes/discussion";
-import replyRoutes from "./src/routes/reply";
+import bodyParser from 'body-parser';
+import Stripe from 'stripe';
 
 import userRoutes from "./src/routes/user";
 
 
 dotenv.config();
-
+const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY || '';
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY || '';
 const app: Express = express();
 const port = process.env.PORT ?? 3001;
 
@@ -23,10 +22,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.use('/api/announcement', announcementRoutes)
 app.use("/api/users", userRoutes);
-app.use("/api/discussions", discussionRoutes);
-app.use("/api/replies", replyRoutes);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
@@ -52,3 +48,154 @@ app.listen(port, () => {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+
+app.use((req, res, next) => {
+  bodyParser.json()(req, res, next);
+});
+
+app.get('/stripe-key', (_, res) => {
+  res.send({publishableKey: stripePublishableKey});
+  return;
+});
+
+app.post('/create-payment-intent', async (req, res) => {
+  const {
+    email = `test${Math.floor(Math.random() * 9999) + 1}@domain.com`,
+    currency,
+    request_three_d_secure,
+    payment_method_types = [],
+  } = req.body;
+
+  const stripe = new Stripe(stripeSecretKey, {
+    // apiVersion: '2020-08-27',
+    typescript: true,
+  });
+
+  const customer = await stripe.customers.create({email});
+
+  const params = {
+    amount: 5000,
+    currency,
+    customer: customer.id,
+    payment_method_options: {
+      card: {
+        request_three_d_secure: request_three_d_secure || 'automatic',
+      },
+    },
+    payment_method_types: payment_method_types,
+  };
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create(params);
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.send({
+      error: "Payment Error!",
+    });
+    return;
+  }
+  return;
+});
+
+app.post('/payment-sheet-setup-intent', async (req, res) => {
+  const {
+    email = `test${Math.floor(Math.random() * 9999) + 1}@domain.com`,
+    payment_method_types = [],
+  } = req.body;
+
+  const stripe = new Stripe(stripeSecretKey, {
+    // apiVersion: '2020-08-27',
+    typescript: true,
+  });
+
+  const customer = await stripe.customers.create({email});
+
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2020-08-27'},
+  );
+  const setupIntent = await stripe.setupIntents.create({
+    ...{customer: customer.id, payment_method_types},
+  });
+
+  res.json({
+    setupIntent: setupIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+  });
+  return;
+});
+
+app.post('/payment-sheet', async (req, res) => {
+  const {email = `test${Math.floor(Math.random() * 9999) + 1}@domain.com`} =
+    req.body;
+
+  const stripe = new Stripe(stripeSecretKey, {
+    // apiVersion: '2020-08-27',
+    typescript: true,
+  });
+
+  const customer = await stripe.customers.create({email});
+
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2020-08-27'},
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 5099,
+    currency: 'usd',
+    payment_method_types: ['card', 'link'],
+  });
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+  });
+  return;
+});
+
+app.post('/payment-sheet-subscription', async (req, res) => {
+  const {email = `test${Math.floor(Math.random() * 9999) + 1}@domain.com`} =
+    req.body;
+
+  const stripe = new Stripe(stripeSecretKey, {
+    // apiVersion: '2020-08-27',
+    typescript: true,
+  });
+
+  const customer = await stripe.customers.create({email});
+
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2020-08-27'},
+  );
+  const PRICE_ID = '<YOUR PRICE ID HERE>';
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{price: PRICE_ID}],
+    trial_period_days: 3,
+  });
+
+  if (typeof subscription.pending_setup_intent === 'string') {
+    const setupIntent = await stripe.setupIntents.retrieve(
+      subscription.pending_setup_intent,
+    );
+
+    res.json({
+      setupIntent: setupIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+    });
+  } else {
+    throw new Error(
+      'Expected response type string, but received: ' +
+        typeof subscription.pending_setup_intent,
+    );
+  }
+  return;
+});
+
+app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
